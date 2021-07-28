e9031c6bfb1f39b8fefc84ba27ebf8094e7d513a
lau <arual.socram@gmail.com>
2021-07-28 09:26:13 +0200

    wip

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import React from "react"
-import Header from "../components/Header"
-import "../App.css";
-import CardsList from "../components/CardsList";
-import AddSuggestion from "../components/AddSuggestion";
-import Footer from "../components/Footer/Footer";
-
-function HomePage(props) {
-  const {dailyPic, setSelectedPic, latestPicsList, user} = props
-  return (
-    <>
-      <Header data={dailyPic}/>
-      <CardsList data={latestPicsList} setSelectedPic={setSelectedPic}/>
-      {user && <AddSuggestion user={user}/>}
-      <Footer/>
-    </>
-  );
-}
-
-export default HomePage;
-=======
 import React from "react"
 import Header from "../components/Header"
 import "../App.css";
@@ -42,5 +20,4 @@ function HomePage(props) {
 }
 
 export default HomePage;
->>>>>>> 251e91b724cfc5a3d7a0c1c9369a825959fef437
   
